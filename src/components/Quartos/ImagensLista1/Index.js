a825c4a411a6e9898ccab0764afcba5853321a25
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2022-01-06 00:35:12 -0300

    FIX: removido o grid-css e usado pos.absolute no li de ImagensLista1

--- src/components/Quartos/ImagensLista1/Index.js
+++ src/components/Quartos/ImagensLista1/Index.js
@@ -12,43 +12,55 @@ export const ImagensLista1 = () => {
             <ul id="quartosLista">
 
                 <li class="quartoListaItem">
+
                     <div class="quartoListaImg">
                         <img src={quarto3} alt="quarto"/>
                     </div>
+
                     <div class="quartoListaInfo">
                         <p>CASAL SIMPLES</p>
                         <VerMaisBtn/>
                     </div>
+
                 </li>
 
                 <li class="quartoListaItem">
+
                     <div class="quartoListaImg">
                         <img src={quarto3} alt="quarto"/>
                     </div>
+
                     <div class="quartoListaInfo">
                         <p>CASAL SIMPLES</p>
                         <VerMaisBtn/>
                     </div>
+                    
                 </li>
 
                 <li class="quartoListaItem">
+
                     <div class="quartoListaImg">
                         <img src={quarto3} alt="quarto"/>
                     </div>
+
                     <div class="quartoListaInfo">
                         <p>CASAL SIMPLES</p>
                         <VerMaisBtn/>
                     </div>
+                    
                 </li>
 
                 <li class="quartoListaItem">
+
                     <div class="quartoListaImg">
                         <img src={quarto3} alt="quarto"/>
                     </div>
+
                     <div class="quartoListaInfo">
                         <p>CASAL SIMPLES</p>
                         <VerMaisBtn/>
                     </div>
+                    
                 </li>
 
             </ul>
