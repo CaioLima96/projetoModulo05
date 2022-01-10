ac52cde878bc615bc00a5cf92045e8e4ffe64c5e
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2022-01-09 22:02:38 -0300

    FIX: Retirado comentários desnecessários

--- src/components/Footer/Index.js
+++ src/components/Footer/Index.js
@@ -40,45 +40,4 @@ const Footer = () => {
     )
 }
 
-// class Footer extends React.Component {
-
-//     render(){
-
-//         return (
-
-//             <FooterTag>
-
-//                 <FooterContent>
-
-//                     <RerserveAquiBtn/>
-
-//                     <div class="divBar"></div>
-
-//                     <ul>
-
-//                         <li>Nossa Localização</li>
-//                         <li>Bairro do Limoeiro</li>
-//                         <li>São Paulo - Brasil</li>
-//                         <li>CEP: 20010-010</li>
-
-//                     </ul>
-
-//                     <div class="divBar"></div>
-
-//                     <ul>
-
-//                         <li>Fale conosco</li>
-//                         <li>Reserva: 4002-8922</li>
-//                         <li>Duvidas: 2569-6969</li>
-//                         <li>Equipe</li>
-
-//                     </ul>
-                    
-//                 </FooterContent>
-
-//             </FooterTag>
-//         )
-//     }
-// }
-
 export default Footer
