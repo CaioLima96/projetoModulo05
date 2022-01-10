c156accfad69de88f83bfc77e70a63ce3c0d19ae
Leo Silva <leonardo.ghost@gmail.com>
2022-01-10 13:19:02 -0300

    Mesclagem

--- src/components/HeroImg/Index.js
+++ src/components/HeroImg/Index.js
@@ -2,12 +2,12 @@ import React from "react";
 
 import { HeroImgContainer } from "./styles";
 
-export const HeroImg = ({children}) => {
+export const HeroImg = ({ children }) => {
     return (
 
         <HeroImgContainer id="heroImg">
 
-            {/* <h1>QUARTOS</h1> */}
+            {children}
 
         </HeroImgContainer>
     )
