49a5146f7588cc31c6cd27662df94c58e402f722
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2022-01-09 21:49:27 -0300

    FIX: Passado ReserveAquiBtn para styled components

--- src/components/ReserveAquiBtn/index.js
+++ src/components/ReserveAquiBtn/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-import './Index.css'
+import { ReserveAquiBtn } from "./styles";
 
 export const RerserveAquiBtn = () => {
     return (
 
-        <button id="reserveAquiBtn">RESERVE AQUI</button>
+        <ReserveAquiBtn id="reserveAquiBtn">RESERVE AQUI</ReserveAquiBtn>
     )
 }
